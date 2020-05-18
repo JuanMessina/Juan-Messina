4cffcba8b4d5e9ea3e4db4f98ffbfba6d36651d4
JuanMessina <j.messina@alumno.um.edu.ar>
2020-05-18 18:17:31 -0300

    prototipo

---
+++ Resuelto/Assets/Scripts/Controllers/ThirdPersonCameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+    public Transform target, player;
+    float mouseX, mouseY;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        CamControl();
+       
+       /* Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        
+        transform.position = smoothedPosition;
+
+        transform.LookAt(player);*/
+    }
+
+   
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+}
